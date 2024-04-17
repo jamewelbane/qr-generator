69a9320d94027155f348420dffb58718bc76e9b5
worldserpentx <142296908+worldserpentx@users.noreply.github.com>
2024-04-17 15:50:53 +0800

    On going, no phpqrcode lib

---
+++ function/generate-qr.php
@@ -0,0 +1,25 @@
+<?php
+require 'phpqrcode/qrlib.php';
+
+// Function to validate URL
+function validateURL($url) {
+    return filter_var($url, FILTER_VALIDATE_URL);
+}
+
+// Check if URL is submitted
+if(isset($_POST['url'])) {
+    $url = $_POST['url'];
+
+    // Validate URL
+    if(validateURL($url)) {
+        // Generate QR code
+        $qrFile = 'qr_codes/'.uniqid().'.png'; // Unique filename for each QR code
+        QRcode::png($url, $qrFile);
+        
+        // Display QR code image
+        echo '<img src="'.$qrFile.'" alt="QR Code">';
+    } else {
+        echo 'Invalid URL. Please enter a valid URL.';
+    }
+}
+?>
